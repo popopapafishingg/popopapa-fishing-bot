69e071136d19d236a0ee43d816d3a67d15a16239
popopapafishingg <b_s@nike.eonet.ne.jp>
2026-04-25 13:59:30 +0900

    Update fishing_report_bot.py

--- fishing_report_bot.py
+++ fishing_report_bot.py
@@ -1,3 +1,4 @@
+print("🔥 THIS IS NEW CODE 🔥")
 import requests
 
 LINE_TOKEN = "IAYqlIVl9Jh6RcvZ5C+YpHmPUv7B7uxLAU89NPSakzeS/25hb/VWjM70OvmihycYxE/C1ssd0nmdTDMTNQzlu6R6NQ95k5ccLZCuFyc19eAh1Gdx8W+hBjezZa0GdKrf+Ej+TIEaYYOd4qQBohYddQdB04t89/1O/w1cDnyilFU="
