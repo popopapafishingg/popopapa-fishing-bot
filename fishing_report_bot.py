cdc6a887100e6a4c4cca15bd34016da5e2b0387a
popopapafishingg <b_s@nike.eonet.ne.jp>
2026-04-25 13:47:52 +0900

    Update fishing_report_bot.py

--- fishing_report_bot.py
+++ fishing_report_bot.py
@@ -1,3 +1,15 @@
+import requests
+
+LINE_TOKEN = "IAYqlIVl9Jh6RcvZ5C+YpHmPUv7B7uxLAU89NPSakzeS/25hb/VWjM70OvmihycYxE/C1ssd0nmdTDMTNQzlu6R6NQ95k5ccLZCuFyc19eAh1Gdx8W+hBjezZa0GdKrf+Ej+TIEaYYOd4qQBohYddQdB04t89/1O/w1cDnyilFU="
+USER_ID = "U7fe227607853d00dc5b4d9614f4761ab"
+
+url = "https://api.line.me/v2/bot/message/push"
+
+headers = {
+    "Authorization": "Bearer " + LINE_TOKEN,
+    "Content-Type": "application/json",
+}
+
 data = {
     "to": USER_ID,
     "messages": [
@@ -7,3 +19,7 @@ data = {
         }
     ],
 }
+
+r = requests.post(url, headers=headers, json=data)
+
+print(r.status_code, r.text)
