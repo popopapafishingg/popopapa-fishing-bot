05bbe1825e0420d2a7d77a3d5a85694308ec5166
popopapafishingg <b_s@nike.eonet.ne.jp>
2026-04-25 14:08:48 +0900

    Update fishing_report_bot.py

--- fishing_report_bot.py
+++ fishing_report_bot.py
@@ -2,9 +2,11 @@ print("🔥 THIS IS NEW CODE 🔥")
 import requests
 
 LINE_TOKEN = "IAYqlIVl9Jh6RcvZ5C+YpHmPUv7B7uxLAU89NPSakzeS/25hb/VWjM70OvmihycYxE/C1ssd0nmdTDMTNQzlu6R6NQ95k5ccLZCuFyc19eAh1Gdx8W+hBjezZa0GdKrf+Ej+TIEaYYOd4qQBohYddQdB04t89/1O/w1cDnyilFU="
-USER_ID = "U7fe227607853d00dc5b4d9614f4761ab"
 
 url = "https://api.line.me/v2/bot/message/push"
+
+# 👇 ここ重要：自分に返信じゃなく broadcast
+url = "https://api.line.me/v2/bot/message/broadcast"
 
 headers = {
     "Authorization": "Bearer " + LINE_TOKEN,
@@ -12,23 +14,14 @@ headers = {
 }
 
 data = {
-    "to": USER_ID,
     "messages": [
         {
             "type": "text",
-            "text": "テスト"
+            "text": "ブロードキャストテスト"
         }
     ],
 }
 
-print("TOKEN length:", len(LINE_TOKEN))
-print("USER_ID:", USER_ID)
-print("USER_ID length:", len(USER_ID))
-print("TEXT:", data["messages"][0]["text"])
-print("TEXT length:", len(data["messages"][0]["text"]))
-print("DATA:", data)
+r = requests.post(url, headers=headers, json=data)
 
-r = requests.post(url, headers=headers, json=data, timeout=30)
-
-print("STATUS:", r.status_code)
-print("RESPONSE:", r.text)
+print(r.status_code, r.text)
