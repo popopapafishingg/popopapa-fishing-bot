5632e088db76cbea8fbdbb029223f1e41b32352c
popopapafishingg <b_s@nike.eonet.ne.jp>
2026-04-25 13:45:58 +0900

    Update fishing_report_bot.py

--- fishing_report_bot.py
+++ fishing_report_bot.py
@@ -1,28 +1,9 @@
-import requests
-
-LINE_TOKEN = "IAYqlIVl9Jh6RcvZ5C+YpHmPUv7B7uxLAU89NPSakzeS/25hb/VWjM70OvmihycYxE/C1ssd0nmdTDMTNQzlu6R6NQ95k5ccLZCuFyc19eAh1Gdx8W+hBjezZa0GdKrf+Ej+TIEaYYOd4qQBohYddQdB04t89/1O/w1cDnyilFU="
-USER_ID = "U7fe227607853d00dc5b4d9614f4761ab"
-
-print("TOKEN length:", len(LINE_TOKEN))
-print("USER_ID:", USER_ID)
-print("USER_ID length:", len(USER_ID))
-
-url = "https://api.line.me/v2/bot/message/push"
-
-headers = {
-    "Authorization": "Bearer " + LINE_TOKEN,
-    "Content-Type": "application/json",
-}
-
 data = {
     "to": USER_ID,
-    "messages": [{"type": "text", "text": "テスト"}],
+    "messages": [
+        {
+            "type": "text",
+            "text": "テスト"
+        }
+    ],
 }
-
-r = requests.post(url, headers=headers, json=data, timeout=30)
-
-print("STATUS:", r.status_code)
-print("RESPONSE:", r.text)
-
-r.raise_for_status()
-print("DONE")
