ffdfd9e3a6fc3abde06282e2cdcf6e2db896f971
popopapafishingg <b_s@nike.eonet.ne.jp>
2026-04-26 11:24:34 +0900

    Update fishing_report_bot.py

--- fishing_report_bot.py
+++ fishing_report_bot.py
@@ -4,14 +4,19 @@ import os
 LINE_TOKEN = os.getenv("POPO_LINE_TOKEN")
 USER_ID = os.getenv("USER_ID")
 
+print("起動OK")
+
 def send(msg):
-    requests.post(
+    print("送信開始")
+    r = requests.post(
         "https://api.line.me/v2/bot/message/push",
         headers={"Authorization": f"Bearer {LINE_TOKEN}"},
         json={"to": USER_ID, "messages": [{"type": "text", "text": msg}]}
     )
+    print("送信結果:", r.status_code, r.text)
 
 def main():
+    print("main入った")
     send("ポポパパAIテスト成功🔥")
 
 if __name__ == "__main__":
